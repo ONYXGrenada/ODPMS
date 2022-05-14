7abced68e3319e44beb85559dd4e610142f6a997
Ozimba SubZero <ozimbat@hotmail.com>
2022-05-14 17:40:04 -0400

    New & Pay ticket functionality added.

--- ODPMS.Shared/Pages/NewTicketContentDialog.xaml.cs
+++ ODPMS.Shared/Pages/NewTicketContentDialog.xaml.cs
@@ -13,6 +13,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using ODPMS.Models;
+using ODPMS.Helpers;
 
 namespace ODPMS.Pages
 {
@@ -26,11 +27,18 @@ namespace ODPMS.Pages
 
 		private void NewTicket_Opened(ContentDialog sender, ContentDialogOpenedEventArgs args)
         {
-			// Create a new ticket object and display on content dialog
+            // Create a new ticket object and display on content dialog
+            NewTicket = DatabaseHelper.CreateTicket();
+
+			this.ticketNumber_txt.Text = NewTicket.Number.ToString();
+            this.ticketDescription_txt.Text = NewTicket.Description.ToString();
+            this.ticketCreated_txt.Text = NewTicket.Created.ToString();
+            this.ticketRate_txt.Text = NewTicket.Rate.ToString();
         }
 		private void PrimaryButton_Clicked(ContentDialog sender, ContentDialogButtonClickEventArgs args)
 		{
 			// Add the new ticket object to the database
+			DatabaseHelper.AddTicket(NewTicket);
 		}
 
 		private void CloseButton_Clicked(ContentDialog sender, ContentDialogButtonClickEventArgs args)
--- ODPMS.Shared/Pages/PayTicketContentDialog.xaml.cs
+++ ODPMS.Shared/Pages/PayTicketContentDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -9,11 +10,15 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using ODPMS.Models;
+using ODPMS.Helpers;
 
 namespace ODPMS.Pages
 {
 	public sealed partial class PayTicketContentDialog : ContentDialog
 	{
+        private Ticket NewTicket;
+        double payAmount;
         public PayTicketContentDialog()
 		{
 			this.InitializeComponent();
@@ -22,26 +27,38 @@ namespace ODPMS.Pages
         private void PayTicket_Opened(ContentDialog sender, ContentDialogOpenedEventArgs args)
         {
             // Get ticket object from database
-            TicketNumberText.Text = "Ticket Number :";
-            TicketStartTimeText.Text = "Start Time :";
-            TicketEndTimeText.Text = "End Time :";
-            TicketDurationText.Text = "Duration :";
-            TicketCostText.Text = "Cost :";
+            NewTicket = DatabaseHelper.FindTicket(3);
 
+            this.TicketNumberText.Text = "Ticket Number: " + NewTicket.Number;
+            this.TicketStartTimeText.Text = "Start Time: " + NewTicket.Created;
+            this.TicketEndTimeText.Text = "End Time: " + NewTicket.Closed;
+            this.TicketDurationText.Text = "Duration: " + NewTicket.Cost / NewTicket.Rate;
+            this.TicketCostText.Text = "Cost: " + NewTicket.Cost;
         }
         private void PrimaryButton_Clicked(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
             // Pay execute the pay function to display change and update ticket in the database
+            //NewTicket.Status = "Paid";
+            DatabaseHelper.PayTicket(NewTicket);
         }
 
         private void CloseButton_Clicked(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-            //D iscard the new ticket object and cancel payment.
+            //Discard the new ticket object and cancel payment.
         }
 
         private void PayAmount_Changed(object sender, RoutedEventArgs e)
-        {
+        {            
+            if (Double.TryParse(this.PaymentAmount.Text,out payAmount))
+            {
+                payAmount = double.Parse(this.PaymentAmount.Text);
+            }
+            else
+            {
+                payAmount = 0.0;
+            }
 
+            this.ChangeReturned.Text = (NewTicket.Cost - payAmount).ToString();
         }
     }
 }
