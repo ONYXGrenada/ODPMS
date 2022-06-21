60ed9708437c21dfe59370fea158ec7e919a541a
Lenus Walker <walker.lenus@gmail.com>
2022-06-20 21:52:35 -0400

    Created initial template to add the cash float. Need the logic.

---
+++ ODPMS.Shared/Dialogs/CashFloatContentDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace ODPMS.Dialogs
+{
+	public sealed partial class CashFloatContentDialog : ContentDialog
+	{
+		public CashFloatContentDialog()
+		{
+			this.InitializeComponent();
+		}
+
+		private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+		{
+		}
+
+		private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+		{
+		}
+	}
+}
