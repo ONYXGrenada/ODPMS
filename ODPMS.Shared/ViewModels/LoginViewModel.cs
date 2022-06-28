90d7d0628973865fd6a31a6e4bdb82b23bfe6fd9
Lenus Walker <walker.lenus@gmail.com>
2022-06-28 19:42:28 -0400

    Update LoginPage to use MVVM for KeyPress. Login function will be attempted when enter is pressed in the password field.

--- ODPMS.Shared/ViewModels/LoginViewModel.cs
+++ ODPMS.Shared/ViewModels/LoginViewModel.cs
@@ -64,7 +64,8 @@ namespace ODPMS.ViewModels
             StatusMessage = "";
         }
 
-        public void EnterKeyDown(KeyRoutedEventArgs e)
+        [ICommand]
+        public void EnterKeyUp(KeyRoutedEventArgs e)
         {
             if (e.Key == Windows.System.VirtualKey.Enter)
                 Login();
